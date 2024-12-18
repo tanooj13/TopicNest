dcfc6a5f5596bee3eed23f1ad80759c398ebe6f0
Tanooj Reddy <142309781+tanooj13@users.noreply.github.com>
2024-12-18 14:52:34 +0530

    clg changes
    clg changes

--- base/models.py
+++ base/models.py
@@ -18,6 +18,9 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        # Newest one will be last with '-' infront of updated
+        ordering = ['-updated','-created']
 
     def __str__(self):
         return self.name
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Room 
+from .forms import RoomForm
 # Create your views here.
 
 
@@ -13,3 +14,37 @@ def room(request,pk):
     room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request,'base/room.html',context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        # Sending all the post data to the form..The RoomForm() knows what fields to extract
+        form = RoomForm(request.POST)
+        if form.is_valid:
+            # Saving the data in the DB
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request,'base/room_form.html',context)
+
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    # The RoomForm will get pre - filled with the room values...If the values dont match it wont work
+    form = RoomForm(instance = room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance= room)
+        if (form.is_valid()):
+            form.save()
+            return redirect('home')
+                                                
+    context = {'form':form}
+    return render(request,'base/room_form.html',context)
+
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id = pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':room})
+    
