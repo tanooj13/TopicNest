ebab9baa152a2e7eeb4498861a085cd0bf437f31
Tanooj Reddy <142309781+tanooj13@users.noreply.github.com>
2024-12-19 15:03:24 +0530

    3rd day

--- base/views.py
+++ base/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Room 
+from django.db.models import Q
+
+from .models import Room,Topic 
 from .forms import RoomForm
 # Create your views here.
 
+def loginPage(request):
+    context = {}
+    return render(request,'base/login_register.html',context)
+
+
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains= q) |
+        Q(name__icontains = q) |
+        Q(description__icontains = q) |
+        Q(host__username__icontains = q)
+    )
+    room_count = rooms.__len__
+    topics = Topic.objects.all()
+    context = {'rooms':rooms,'topics':topics,'room_count':room_count}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
@@ -47,4 +63,6 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     return render(request,'base/delete.html',{'obj':room})
+
+
     
