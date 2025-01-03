4029551b28a154335ea3a167f9f2c3004db93f3e
Tanooj Reddy <142309781+tanooj13@users.noreply.github.com>
2025-01-03 21:52:24 +0530

    commit-5:15

--- base/views.py
+++ base/views.py
@@ -101,33 +101,46 @@ def userProfile(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic,created = Topic.objects.get_or_create(name = topic_name)
         # Sending all the post data to the form..The RoomForm() knows what fields to extract
-        form = RoomForm(request.POST)
-        if form.is_valid:
-            # Saving the data in the DB
-            room = form.save(commit=False)
-            room.host = request.user
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description')
+        )
+        # form = RoomForm(request.POST)
+        # if form.is_valid:
+        #     # Saving the data in the DB
+        #     room = form.save(commit=False)
+        #     room.host = request.user
             
-            room.save()
-            return redirect('home')
-    context = {'form':form}
+        #    room.save()
+        return redirect('home')
+    context = {'form':form,"topics":topics}
     return render(request,'base/room_form.html',context)
 
 @login_required(login_url='login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
+    topics = Topic.objects.all()
     # The RoomForm will get pre - filled with the room values...If the values dont match it wont work
     form = RoomForm(instance = room)
     if request.user != room.host:
         return HttpResponse('Only the creater can edit the room')
     if request.method == 'POST':
-        form = RoomForm(request.POST,instance= room)
-        if (form.is_valid()):
-            form.save()
-            return redirect('home')
-                                                
-    context = {'form':form}
+        topic_name = request.POST.get('topic')
+        topic,created = Topic.objects.get_or_create(name = topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+    up  = True                       
+    context = {'form':form,"topics":topics,"up":up,"room":room}
     return render(request,'base/room_form.html',context)
 
 @login_required(login_url='login')
@@ -150,4 +163,8 @@ def deleteMessage(request,pk):
         message.delete()
         return redirect('home')
     return render(request,'base/delete.html',{'obj':message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    return render(request,'base/update-user.html')
     
